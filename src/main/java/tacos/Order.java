272e80b13bb3d9b25ad1479006ae17c0a891bdd8
Lucas Madeira <lmadeira.ec@gmail.com>
2023-03-30 11:51:41 -0400

    adicionando repositorios jpa

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,8 +1,10 @@
 package tacos;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Pattern;
 
@@ -13,13 +15,17 @@ import lombok.Data;
 
 //tag::newFields[]
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
+    private static final long   serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
-
-//end::newFields[]
 
     @NotBlank(message="Delivery name is required")
     private String deliveryName;
@@ -46,18 +52,16 @@ public class Order {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
-    public void addDesign(Taco design) {
-        this.tacos.add(design);
+    public void addDesign(Taco desing){
+        this.tacos.add(desing);
     }
 
-  /*
-// tag::newFields[]
-  ...
-
-// end::newFields[]
-   */
-//tag::newFields[]
+    @PrePersist
+    void placedAt(){
+        this.placedAt = new Date();
+    }
 }
 //end::newFields[]
