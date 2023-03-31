bc209a78940310c0325bc524031b09ad605f6fe2
Lucas Madeira <lmadeira.ec@gmail.com>
2023-03-31 15:12:34 -0400

    Evoluindo OrderRepository e adicionando dependencia do spring security no pom.xml

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,5 +3,13 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order,Long> {
+    List<Order> findByDeliveryZip(String deliveryZip);
+
+    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+
 }
